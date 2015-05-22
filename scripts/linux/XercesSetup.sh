79f876d2ca9eba5fbc03f5e9a63ddb5e980fb454
Ovidiu Parvu <IceRage@users.noreply.github.com>
2015-05-22 20:45:06 +0100

    Update XercesSetup.sh
    Do not remove source files after installation.

--- scripts/linux/XercesSetup.sh
+++ scripts/linux/XercesSetup.sh
@@ -60,8 +60,5 @@ sudo make install
 # Return to the parent directory
 cd ../../
 
-# Remove all source files
-rm -rf ${FOLDER_NAME}
-
 # Inform user that Xerces was successfully installed
 echo "Xerces was successfully installed."
